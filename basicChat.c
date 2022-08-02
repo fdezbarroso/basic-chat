1226764c45c745818d61b3f82ba2c3efa2245fac
fdezbarroso <f.fdezbarroso@gmail.com>
2022-08-02 18:01:17 +0200

    Implement basic network code for server and client

--- basicChat.c
+++ basicChat.c
@@ -12,8 +12,6 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-#define PORT "26000"
-
 int main()
 {
     DWORD dwThreadIdArray[2];
@@ -77,16 +75,153 @@ int main()
 
 DWORD WINAPI chatServer(LPVOID lpParam)
 {
-    char* param = lpParam;
+    SOCKET serverSockfd, clientSockfd;
+    struct sockaddr_in servAddr, aux;
+    u_short serverPort;
+    socklen_t auxLen;
+    int iResult;
+
+    char confirmationMsg[50];
+
+    char *param = lpParam;
     printf("Server Thread, param: %s\n", param);
+
+    /* Create socket */
+    serverSockfd = INVALID_SOCKET;
+    if ((serverSockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+    {
+        printf("server socket() failed, error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return -1;
+    }
+
+    /* Define server address */
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    // TODO: Change to htons(0)
+    servAddr.sin_port = htons(0);
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    /* Bind socket */
+    if ((iResult = bind(serverSockfd, (struct sockaddr *)&servAddr, sizeof(servAddr))) == SOCKET_ERROR)
+    {
+        printf("server bind() failed, error: %ld\n", WSAGetLastError());
+        closesocket(serverSockfd);
+        WSACleanup();
+        return -1;
+    }
+
+    /* Get server socket information */
+    auxLen = sizeof(aux);
+    getsockname(serverSockfd, (struct sockaddr *)&aux, &auxLen);
+    serverPort = ntohs(aux.sin_port);
+    printf("Server port: %u\n", serverPort);
+
+    /* Listen on socket */
+    if ((iResult = listen(serverSockfd, 1)) == SOCKET_ERROR)
+    {
+        printf("server listen() failed, error: %ld\n", WSAGetLastError());
+        closesocket(serverSockfd);
+        WSACleanup();
+        return -1;
+    }
+
+    /* Accept connection */
+    clientSockfd = INVALID_SOCKET;
+    if ((clientSockfd = accept(serverSockfd, NULL, NULL)) == INVALID_SOCKET)
+    {
+        printf("server accept() failed, error: %ld\n", WSAGetLastError());
+        closesocket(serverSockfd);
+        WSACleanup();
+        return -1;
+    }
+    // TODO: Show connection information
+
+    /* Send connection confirmation */
+    sprintf(confirmationMsg, "%s: Connection stablished!", lpParam);
+    if ((iResult = send(clientSockfd, confirmationMsg, sizeof(confirmationMsg), 0)) == SOCKET_ERROR)
+    {
+        printf("confirmation send() failed, error: %ld\n", WSAGetLastError());
+        closesocket(serverSockfd);
+        closesocket(clientSockfd);
+        WSACleanup();
+        return -1;
+    }
+
+    /* TODO: START CHAT */
+
+    /* Disconnect */
+    closesocket(serverSockfd);
+    closesocket(clientSockfd);
+    WSACleanup();
 
     return 0;
 }
 
 DWORD WINAPI chatClient(LPVOID lpParam)
 {
-    char* param = lpParam;
+    SOCKET clientSockfd;
+    struct sockaddr_in clientAddr;
+    u_short serverPort;
+    int iResult;
+
+    char confirmationMsg[50];
+
+    char *param = lpParam;
     fprintf(stdout, "Client Thread, param: %s\n", param);
+
+    /* Create socket */
+    clientSockfd = INVALID_SOCKET;
+    if ((clientSockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+    {
+        printf("client socket() failed, error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return -1;
+    }
+
+    /* User input port used */
+    Sleep(500);
+    printf("Please enter the port number you wish to connect to:\n");
+    scanf("%u", &serverPort);
+
+    /* Define server address */
+    memset(&clientAddr, 0, sizeof(clientAddr));
+    clientAddr.sin_family = AF_INET;
+    // TODO: htons(serverPort)
+    clientAddr.sin_port = htons(serverPort);
+    // TODO: let user modify IP
+    clientAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    /* Connect */
+    if ((iResult = connect(clientSockfd, (struct sockaddr *)&clientAddr, sizeof(clientAddr))) == SOCKET_ERROR)
+    {
+        printf("client connect() failed, error: %ld\n", WSAGetLastError());
+        closesocket(clientSockfd);
+        WSACleanup();
+        return -1;
+    }
+
+    iResult = recv(clientSockfd, confirmationMsg, sizeof(confirmationMsg), 0);
+    if (iResult < 0)
+    {
+        printf("confirmation recv() failed, error: %ld\n", WSAGetLastError());
+    }
+    else if (iResult == 0)
+    {
+        printf("Connection closed\n");
+    }
+    else
+    {
+        /* Print connection confirmation */
+        printf("%s\n", confirmationMsg);
+
+        /* TODO: START CHAT */
+
+    }
+
+    /* Disconnect */
+    closesocket(clientSockfd);
+    WSACleanup();
 
     return 0;
 }
